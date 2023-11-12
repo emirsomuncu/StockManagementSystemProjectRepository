6bbdeccd620783a142a026f6886937ae8e7b1f7e
emirsomuncu <somuncuemir55@gmail.com>
2023-11-12 23:28:49 +0300

    db locale alındı proje sorunsuz

--- src/Database/DbHelper.java
+++ src/Database/DbHelper.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbHelper {
-    public static String USERNAME = "sql12658917";
-    public static String PASSWORD = "gqSrjLqaQq";
-    public static String DB_CONNECTION_URL = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12658917";
+    public static String USERNAME = "root";  // These infos for our computers . You need to configure it for yourself
+    public static String PASSWORD = "";  // These infos for our computers . You need to configure it for yourself
+
+    public static String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/stock_management_db";
 
     public static Connection connectToDatabase() {
         Connection connection = null;
